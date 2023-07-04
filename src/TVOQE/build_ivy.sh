6339951fc5c6cee16839f713463c1ba4d42008b1
ZuoYuanP <92987321+ZuoYuanP@users.noreply.github.com>
2023-07-04 22:59:41 +0800

    Update build_ivy.sh

--- src/TVOQE/build_ivy.sh
+++ src/TVOQE/build_ivy.sh
@@ -146,5 +146,18 @@ cd $test
 sudo cp -r $buildivy/src/var $test/var
 
 
+# Add .ivy file of the quic project directly in "include" folder of the ivy python library 
+array=()
+while IFS=  read -r -d $'\0'; do
+    array+=("$REPLY")
+done < <(find $test/QUIC-Ivy/doc/examples/quic -type f -name \*.ivy -print0)
 
+echo $array
 
+SUB='test'
+for j in "${array[@]}"; do : 
+    if [[ ! "$j" == *"$SUB"* ]]; then
+	printf "Files => $j  \n" 
+    	sudo cp $j /usr/local/lib/python2.7/dist-packages/ivy/include/1.7
+    fi
+done
